dbf35a2a410f7038ef69f292692a08fefb0ed2f4
yannunes <yannunes@gmail.com>
2017-06-19 10:43:38 -0300

    updates first

--- install_mining_node.sh
+++ install_mining_node.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
+sudo apt-get update
 sudo apt-get install -y git libmicrohttpd-dev libssl-dev cmake build-essential
 git clone https://github.com/fireice-uk/xmr-stak-cpu.git
 cd xmr-stak-cpu
