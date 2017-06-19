2814c66eff8c6cd099a506fc1a05a5ff88d2e82e
yannunes <yannunes@gmail.com>
2017-06-19 10:11:32 -0300

    URL fix

--- install_mining_node.sh
+++ install_mining_node.sh
@@ -8,4 +8,4 @@ cmake .
 sudo make install
 cd bin
 rm config.txt
-wget "https://github.com/midyan/monero-files/blob/master/lib/$1core/config.txt" -O config.txt
+wget "https://raw.githubusercontent.com/midyan/monero-files/master/lib/$1core/config.txt" -O config.txt
