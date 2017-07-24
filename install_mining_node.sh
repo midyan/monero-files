25b38bc856a8b71606b1b539d1796cd38016febb
yannunes <yannunes@gmail.com>
2017-07-24 13:24:13 -0300

    -y

--- install_mining_node.sh
+++ install_mining_node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
+sudo apt-get install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev -y
 git clone https://github.com/fireice-uk/xmr-stak-cpu.git
 cd xmr-stak-cpu
 cmake .
